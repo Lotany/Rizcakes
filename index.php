e58627118cb6deb5eabbfbcf92fc123bf997be52
Able <ablekilinda@gmail.com>
2022-11-03 13:47:49 +0300

    lotan

--- index.php
+++ index.php
@@ -35,7 +35,7 @@
     <span class="dot"></span>
   </div>
 
-  
+
 
   <div class="login-fom">
     <?php if (isset($_GET['logout']))
@@ -87,6 +87,26 @@
     </div>
 
 
+  </div>
+
+
+
+
+  <div class="w3-row-padding" id="about">
+    <div class="w3-center w3-padding-64">
+      <span class="w3-xlarge w3-bottombar w3-border-dark-grey w3-padding-16">Who We Are</span>
     </div>
+    <div class="w3-third w3-margin-bottom">
+      <div class="w3-card-4">
+        <img src="folder/1656582503596.jpg" alt="John" style="width:100%">
+        <div class="w3-container">
+          <h3>Jane Doe</h3>
+          <p class="w3-opacity">CEO & Founder</p>
+          <p>Phasellus eget enim eu lectus faucibus vestibulum. Suspendisse sodales pellentesque elementum.</p>
+          <p><button class="w3-button w3-light-grey w3-block">Contact</button></p>
+        </div>
+      </div>
+    </div>
+  </div>
 
     <?php include 'footer.php' ?>
