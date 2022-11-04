0eb399442e40e758b654783a09a3c6d1673c0993
Able <ablekilinda@gmail.com>
2022-11-04 15:36:00 +0300

    fixind home area

--- index.php
+++ index.php
@@ -11,19 +11,19 @@
   <div class="slideshow-container">
     <div class="mySlides fade">
       <div class="numbertext">1 / 3</div>
-      <img src="folder/1656582503596.jpg" style="width:100%">
+      <img src="folder/1656582503596.jpg" style="max-width:50%">
       <div class="text">Caption Text</div>
     </div>
 
     <div class="mySlides fade">
       <div class="numbertext">2 / 3</div>
-      <img src="folder/1656582611202.jpg" style="width:100%">
+      <img src="folder/1656582611202.jpg" style="max-width:50%">
       <div class="text">Caption Two</div>
     </div>
 
     <div class="mySlides fade">
       <div class="numbertext">3 / 3</div>
-      <img src="folder/20221013_150111.jpg" style="width:100%">
+      <img src="folder/20221013_150111.jpg" style="width:50%">
       <div class="text">Caption Three</div>
     </div>
   </div>
