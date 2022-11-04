475725a9fb49bbb72d786be2ec4da47126293e0a
Able <ablekilinda@gmail.com>
2022-11-04 18:14:22 +0300

    positioning elements

--- index.php
+++ index.php
@@ -51,7 +51,7 @@
     <div class="w3-third w3-margin-bottom">
       <div class="w3-card-4">
         <?php foreach ($conte as $cont) : ?>
-          <img src="./folder/<?php echo $cont['file']; ?>" alt="John" style="width:50%">
+          <img src="./folder/<?php echo $cont['file']; ?>" alt="John" style="width:100%">
           <div class="w3-container">
             <h3><?php echo $cont['name']; ?></h3>
             <p class="w3-opacity">CEO & Founder</p>
