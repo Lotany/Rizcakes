73a2f6edfa4ef3922ad413fee3bb62400336f72b
Able <ablekilinda@gmail.com>
2022-11-05 10:37:50 +0300

    adding styling

--- index.php
+++ index.php
@@ -5,12 +5,19 @@
 <?php $conte = getposts(); ?>
 <?php include 'header.php' ?>
 
-<div class="wrapper">
-  
 <!--displayed errors here -->
 <?php include 'errors.php' ?>
 <?php include 'login.php' ?>
-  <!--end of errors -->
+<!--end of errors -->
+<div class="row">
+  <div class="leftcolumn">
+    <div class="card">
+      <h2> heading 1</h2>
+      <img src="folder/1656582503596.jpg" class="fakeimg1" style="height: 200px;">
+      <h5>Tittle description, dec 5, 2020</h5>
+    </div>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta quod aspernatur ipsa deserunt esse libero iure. Quibusdam, molestias consectetur omnis rem eaque nostrum cum aliquam harum, ducimus alias tenetur asperiores?</p>
+  </div>
+</div>
 
-
-  <?php include 'footer.php' ?>
+<?php include 'footer.php' ?>
