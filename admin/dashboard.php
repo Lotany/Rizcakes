67e30d4b551cae101fed9abdacfe1ec4f4210e0d
Able <ablekilinda@gmail.com>
2022-11-05 07:21:13 +0300

    lotan

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -6,41 +6,17 @@ if(!isset($_SESSION["id"])){
 <html>
     <head>
         <title>Admin</title>
-        <link rel="stylesheet" href="style.css">
         <meta name="viewport" content="width=device-width,initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
     </head>
-
     <body>
-        <h1>Admin site</h1>
-        <?php if(isset($_GET['login'])) $msg ="Login successfull"; ?>
-        <p><?php if(isset($msg)) echo $msg ?></p>
-        <p>Go back <a href="../index.php">Home</a></p>
-
-        <?php if(isset($_GET['upload'])) $msg ="Post uploaded successfull"; ?>
-        <p><?php if(isset($msg)) echo $msg ?></p>
-        
-
-        <div class="sess">
-        <h3><?php echo $_SESSION['username'] ?></h3>
-        <i><a href="logout.php">logout</a></i>
-        </div>
-
-  <div class="fom">
-        <form action="dashboad.php" method="POST" enctype="multipart/form-data">
-            <label for="name">Name</label>
-            <input type="text" name="name">
-            <input type="file" name="uploadfile">
-            <label for="para">Content</label>
-            <input type="text" name="cont">
-            <label for="link">Link</label>
-            <input type="text" name="link">
-            <button type="submit" name="submit">Upload</button>
-        </form>
-    </div>
-
-
     <div class="wrapper">
         <div class="sidebar">
+            <div class="profile">
+                <img src="../banner.jpg" alt="profile picture">
+                <h3>Lotan Kilinda</h3>
+                <p>Designer</p>
+            </div>
 
         </div>
     </div>
